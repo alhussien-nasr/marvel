6e006d4a6b3f555c503b55643ed794bc75f2fc85
hussein <alhussien.n.g@gmail.com>
2021-07-30 00:35:40 +0200

    Updated Home

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,58 +1,35 @@
 import React from "react";
-import {
-  View,
-  FlatList,
-  Image,
-  SafeAreaView,
-  StyleSheet,
-  TouchableOpacity,
-} from "react-native";
-import Icon from "react-native-vector-icons/MaterialIcons";
-import { images } from "../../assets";
-import { CharacterCard } from "../../components";
+import { FlatList, View } from "react-native";
+import { CharacterCard, Screen, Header } from "../../components";
 import { useCharcter } from "../../hooks/useResult";
 
 export const Home = ({ navigation }) => {
-  const [results] = useCharcter();
+  const { results, loading } = useCharcter();
+  const renderItem = ({ item }) => {
+    const { name, thumbnail, id } = item;
+    const source = thumbnail.path + "." + thumbnail.extension;
+    return (
+      <CharacterCard
+        onPress={() => navigation.navigate("CharacterDetails", { id })}
+        {...{ name, source }}
+      />
+    );
+  };
+
   return (
-    <SafeAreaView style={styles.saveView}>
-      <View style={styles.viewStyle}>
-        <Image style={styles.imgStyle} source={images.logo} />
-        <TouchableOpacity
-          onPress={() => {
-            navigation.navigate("Search");
-          }}
-        >
-          <Icon name="search" color="red" size={40} width={230} height={200} />
-        </TouchableOpacity>
-      </View>
+    <Screen loading={loading}>
+      <Header onSearch={() => navigation.navigate("Search")} />
       <FlatList
         data={results}
-        renderItem={({ item }) => {
-          return (
-            <CharacterCard
-              item={item}
-              title={item.name}
-              source={item.thumbnail.path + "." + item.thumbnail.extension}
-            />
-          );
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={renderItem}
+        showsVerticalScrollIndicator={false}
+        ItemSeparatorComponent={() => <View style={{ height: 20 }} />}
+        contentContainerStyle={{
+          alignItems: "center",
+          paddingVertical: 20,
         }}
       />
-    </SafeAreaView>
+    </Screen>
   );
 };
-const styles = StyleSheet.create({
-  saveView: { backgroundColor: "black", flex: 1, alignItems: "center" },
-  viewStyle: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-around",
-    width: "100%",
-    height: "15%",
-    backgroundColor: "#242424",
-    borderWidth: 1,
-    borderBottomRightRadius: 30,
-    borderBottomLeftRadius: 30,
-  },
-  imgStyle: { width: 170, height: 40, marginLeft: 60 },
-});
