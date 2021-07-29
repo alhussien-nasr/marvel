4f454a72a6f5d3283f03c82b94f03831d3e7ffd5
hussein <alhussien.n.g@gmail.com>
2021-07-30 00:23:38 +0200

    Updated CharacterCard Component

--- src/components/CharacterCard/index.js
+++ src/components/CharacterCard/index.js
@@ -1,40 +1,45 @@
-import React from 'react';
-import {
-  ImageBackground,
-  Text,
-  View,
-  StyleSheet,
-  TouchableOpacity,
-} from 'react-native';
-import {useNavigation} from '@react-navigation/native'
-export const CharacterCard = ({title, source , item}) => {
-    const navigation=useNavigation()
+import React from "react";
+import { Image, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { BlurView } from "expo-blur";
+export const CharacterCard = ({ source, name, onPress }) => {
   return (
-    <TouchableOpacity style={{marginBottom:30}} onPress={()=>{ navigation.navigate("CharacterDetails", { id : item.id })}} >
-      <ImageBackground source={{uri:source}} resizeMode="stretch" style={styles.container}>
-        <View style={styles.container}>
-          <View style={styles.viewOne}>
-            <Text style={styles.textStyle}>{title}</Text>
-          </View>
-        </View>
-      </ImageBackground>
+    <TouchableOpacity style={styles.container} onPress={onPress}>
+      <Image
+        source={{ uri: source }}
+        resizeMode="cover"
+        style={styles.imgStyle}
+      />
+      <BlurView intensity={80} style={styles.viewOne}>
+        <Text style={styles.textStyle}>{name}</Text>
+      </BlurView>
     </TouchableOpacity>
   );
 };
 const styles = StyleSheet.create({
   container: {
     width: 350,
+    height: 140,
+    borderRadius: 25,
+    overflow: "hidden",
+  },
+  imgStyle: {
+    width: 350,
     height: 150,
     borderRadius: 30,
-    overflow: 'hidden',
-    flexDirection: 'column-reverse',
   },
   viewOne: {
-    width: '100%',
-    height: '25%',
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#2F2323',
+    width: 350,
+    height: 35,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#2F2323",
+    position: "absolute",
+    bottom: 0,
+    zIndex: 2,
   },
-  textStyle: {color: 'white', fontSize: 25, fontWeight: '600'},
+  textStyle: {
+    color: "white",
+    fontSize: 25,
+    fontWeight: "600",
+  },
 });
