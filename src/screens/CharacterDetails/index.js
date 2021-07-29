142795506a006a21241cae624a3f7dc5dd6788d2
hussein <alhussien.n.g@gmail.com>
2021-07-30 00:31:48 +0200

    Updated CharacterDetails Screen

--- src/screens/CharacterDetails/index.js
+++ src/screens/CharacterDetails/index.js
@@ -1,14 +1,6 @@
 import React from "react";
-import {
-  View,
-  SafeAreaView,
-  ScrollView,
-  Image,
-  Text,
-  FlatList,
-  StyleSheet,
-} from "react-native";
-import { ComicCard } from "../../components";
+import { ScrollView, Image, Text, StyleSheet } from "react-native";
+import { Screen, Card } from "../../components";
 import {
   useCharcterId,
   useComics,
@@ -18,103 +10,39 @@ import {
 } from "../../hooks/useResult";
 export const CharacterDetails = ({ route }) => {
   const { id } = route.params;
-  const [result] = useCharcterId(id);
-  const [comic] = useComics(id);
-  const [event] = useEvent(id);
-  const [series] = useSeries(id);
-  const [stories] = useStories(id);
-
-  if (!result) {
-    return null;
-  }
-
+  const [result, loadingCharcter] = useCharcterId(id);
+  const [comic, loadingComics] = useComics(id);
+  const [event, loadingEvent] = useEvent(id);
+  const [series, loadingSeries] = useSeries(id);
+  const [stories, loadingStories] = useStories(id);
+  // const noComicsData = comic?.length == 0;
   return (
-    <SafeAreaView style={styles.safeViewStyle}>
-      <ScrollView>
-        <View>
-          <Image
-            source={{ uri: result[0].thumbnail.path + ".jpg" }}
-            style={styles.imgStyle}
-          />
-          <Text style={styles.textOneStyle}>{result[0].name}</Text>
+    <Screen loading={loadingCharcter}>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <Image
+          source={{ uri: result[0]?.thumbnail.path + ".jpg" }}
+          style={styles.imgStyle}
+          resizeMode="cover"
+        />
+        <Text style={styles.textOneStyle}>{result[0]?.name}</Text>
+        {result[0]?.description ? (
           <Text style={styles.textTwoStyle}>descrtiption</Text>
-          <Text style={{ color: "white" }}>{result[0].description}</Text>
-        </View>
-        <View>
-          <Text style={styles.textThreeStyle}>comics</Text>
-          <FlatList
-            horizontal
-            data={comic}
-            renderItem={({ item }) => {
-              return (
-                <ComicCard
-                  imageDetails={
-                    item.thumbnail.path + "." + item.thumbnail.extension
-                  }
-                  textDetails={item.title}
-                />
-              );
-            }}
-          />
-        </View>
-        <View>
-          <Text style={styles.textThreeStyle}>Events</Text>
-          <FlatList
-            horizontal
-            data={event}
-            renderItem={({ item }) => {
-              return (
-                <ComicCard
-                  imageDetails={
-                    item.thumbnail.path + "." + item.thumbnail.extension
-                  }
-                  textDetails={item.title}
-                />
-              );
-            }}
-          />
-        </View>
-        <View>
-          <Text style={styles.textThreeStyle}>series</Text>
-          <FlatList
-            horizontal
-            data={series}
-            renderItem={({ item }) => {
-              return (
-                <ComicCard
-                  imageDetails={
-                    item.thumbnail.path + "." + item.thumbnail.extension
-                  }
-                  textDetails={item.title}
-                />
-              );
-            }}
-          />
-        </View>
-        <View>
-          <Text style={styles.textThreeStyle}>Stories</Text>
-          <FlatList
-            horizontal
-            data={stories}
-            renderItem={({ item }) => {
-              console.log(item.thumbnail);
-
-              return (
-                <ComicCard
-                  imageDetails={item.thumbnail + "." + item.thumbnail}
-                  textDetails={item.title}
-                />
-              );
-            }}
-          />
-        </View>
+        ) : null}
+        <Text style={{ color: "white" }}>{result[0]?.description}</Text>
+        <Card loading={loadingComics} title={"comics"} data={comic} />
+        <Card data={event} title={"events"} loading={loadingEvent} />
+        <Card data={series} title={"series"} loading={loadingSeries} />
+        <Card data={stories} title={"Stories"} loading={loadingStories} />
       </ScrollView>
-    </SafeAreaView>
+    </Screen>
   );
 };
 const styles = StyleSheet.create({
-  safeViewStyle: { flex: 1, backgroundColor: "black" },
-  imgStyle: { width: "100%", height: 250, borderRadius: 15 },
+  imgStyle: {
+    width: "100%",
+    height: 250,
+    borderRadius: 15,
+  },
   textOneStyle: {
     fontSize: 40,
     fontWeight: "700",
@@ -122,11 +50,9 @@ const styles = StyleSheet.create({
     marginTop: 20,
     marginBottom: 20,
   },
-  textTwoStyle: { fontSize: 20, color: "#C44A4A", marginBottom: 10 },
-  textThreeStyle: {
-    color: "#C44A4A",
+  textTwoStyle: {
     fontSize: 20,
-    marginTop: 20,
+    color: "#C44A4A",
     marginBottom: 10,
   },
 });
